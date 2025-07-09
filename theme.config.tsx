457a1dfa6311bc6da0b2a063fab0fd5dda1c3604
Deborah Yuen <deborahyuen2016@berkeley.edu>
2025-07-09 15:12:54 -0700

    create page header

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,15 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Spatial Design with the Apple Vision Pro @ SIGGRAPH</span>,
+    banner: {
+    key: 'announcement-2025',
+    text: (
+      <a href="https://siggraph2025.org" target="_blank" rel="noreferrer">
+        🎉 Join us at SIGGRAPH on Monday, August 11th 2025 from 2:00-3:30 pm at West Building, Exhibit Hall B →
+      </a>
+    ),
+    dismissible: true
+  },
   project: {
     link: 'https://github.com/debbieyuen/spatialui',
   },
